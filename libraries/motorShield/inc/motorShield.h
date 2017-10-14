3471723f27a1e20a207e55fede49b91c9b56d0c7
Sebastian Wetzel <seba.wetzel@gmail.com>
2017-10-14 20:46:38 -0300

    .

--- libraries/motorShield/inc/motorShield.h
+++ libraries/motorShield/inc/motorShield.h
@@ -3,14 +3,34 @@
 
 #include "arduino.h"
 
+// Bit positions in the 74HCT595 shift register output
+#define MOTOR1_A 2
+#define MOTOR1_B 3
+#define MOTOR2_A 1
+#define MOTOR2_B 4
+#define MOTOR4_A 0
+#define MOTOR4_B 6
+#define MOTOR3_A 5
+#define MOTOR3_B 7
+
+
+
+
 typedef struct{
-  port_u puerto;
+  port_u port;
   uint8_t pin
 } pins_u;
 
-//Prototipos de funciones
+typedef struct{
+  pins_u data;
+  pins_u clock;
+  pins_u latch
+} sipo_u;
 
-void shiftOut(pins_u dataPin, pins_u clockPin, uint8_t val);
+
+//Prototipos de funciones
+void shieldInit(sipo_u);
+void shiftOut(sipo_u , uint8_t val);
 
 
 
--- libraries/motorShield/src/motorShield.c
+++ libraries/motorShield/src/motorShield.c
@@ -1,14 +1,17 @@
 #include "motorShield.h"
 
+void shieldInit(sipo_u sipo){
+	pinMode(sipo.data.port,  sipo.data.pin, OUTPUT);
+	pinMode(sipo.clock.port, sipo.clock.pin,OUTPUT);
+	pinMode(sipo.latch.port, sipo.latch.pin, OUTPUT);
+}
 
-
-
-void shiftOut(pins_u dataPin, pins_u clockPin, uint8_t val){
-		for (uint8_t i = 0; i < 8; i++)  {
-			digitalWrite(dataPin.puerto, dataPin.pin, !!(val & (1 << i)));   //Nos desplazamos por el byte, bit pot bit
-
-
-		digitalWrite(clockPin.puerto, clockPin.pin, HIGH);
-		digitalWrite(clockPin.puerto, clockPin.pin, LOW);
-	}
+void shiftOut(sipo_u sipo, uint8_t val){
+								for (uint8_t i = 0; i < 8; i++)  {
+																digitalWrite(sipo.data.port, sipo.data.pin, !!(val & (1 << i))); //Nos desplazamos por el byte, bit pot bit
+																digitalWrite(sipo.clock.port, sipo.clock.pin, HIGH);
+																digitalWrite(sipo.clock.port, sipo.clock.pin, LOW);
+								}
+								digitalWrite(sipo.latch.port, sipo.latch.pin, HIGH);
+								digitalWrite(sipo.latch.port, sipo.latch.pin, LOW);
 }
