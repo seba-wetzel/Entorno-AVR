5a7be7e44d3c5752f44cc03ccf4c82d611a2ca96
Lucas Gimenez <lucas07gimenez@gmail.com>
2017-10-17 00:02:17 -0300

    Se separaron los drivers en sus respectivos archivos .c y .h

---
+++ drivers/inc/uart.h
@@ -0,0 +1,21 @@
+// AVR Includes
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+// Standard Includes
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#ifndef UART0_H_
+#define UART0_H_
+
+int uart_putchar(char c, FILE *stream);
+int uart_getchar(FILE *stream);
+void serialBegin(void);
+extern FILE uart_output;
+extern FILE uart_input;
+
+#endif
+
